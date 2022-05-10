601c46e2f05a3f9ded09803d7b879e468c2fb9fb
sbvorpagel <sbvorpagel@gmail.com>
2022-05-10 17:22:37 -0400

    Added JavaScript solution for code 1003

--- code/1003/1003.js
+++ code/1003/1003.js
@@ -1,11 +1,7 @@
 const input = require('fs').readFileSync('/dev/stdin', 'utf8');
 const lines = input.split('\n');
 
-const n = 3.14159;
-const R = parseFloat(lines[0]);
+const num1 = parseInt(lines[0]);
+const num2 = parseInt(lines[1]);
 
-const A = n * (R*R)
-
-const roundNumber = ((Math.round(A*10000))/10000).toString().split('.');
-
-console.log(`A=${roundNumber[0]}.${roundNumber[1].padEnd(4, "0")}`);
+console.log(`SOMA = ${num1 + num2}`);
