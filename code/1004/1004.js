a0794a9e01c5c782747002c35831de577203c4af
sbvorpagel <sbvorpagel@gmail.com>
2022-05-11 09:04:03 -0400

    Added 1004 JavaScript solution

---
+++ code/1004/1004.js
@@ -0,0 +1,7 @@
+const input = require('fs').readFileSync('/dev/stdin', 'utf8');
+const lines = input.split('\n');
+
+const num1 = parseInt(lines[0]);
+const num2 = parseInt(lines[1]);
+
+console.log(`PROD = ${num1 * num2}`);
