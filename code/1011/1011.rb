ff6663a2fa014f5409515abdd08906d56200d2df
sbvorpagel <sbvorpagel@gmail.com>
2022-05-12 11:44:24 -0400

    Added 1011 JavaScript, C, CPP and Ruby solution

---
+++ code/1011/1011.rb
@@ -0,0 +1,6 @@
+PI = 3.14159
+
+r = gets.to_f
+
+puts format('VOLUME = %.3f', (4.0 / 3.0) * PI * r**3)
+
--- docker/judge/judge_c.rb
+++ docker/judge/judge_c.rb
@@ -4,7 +4,7 @@ def judgeC(path, name)
   code_path = "#{path}/#{name}.c"
 
   if File.exist?(code_path)
-    `gcc #{code_path} -o #{path}/#{name}`
+    `gcc #{code_path} -o #{path}/#{name} -lm`
     check(path, 'C', "./#{path}/#{name}")
     `rm -rf #{path}/#{name}`
   end
