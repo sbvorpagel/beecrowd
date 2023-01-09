5c76d3ab2757400e8f5ee8e62bdf76a6b737dd14
sbvorpagel <sbvorpagel@gmail.com>
2023-01-09 07:51:50 -0400

    Prettier some code

--- code/1052/1052.js
+++ code/1052/1052.js
@@ -2,16 +2,16 @@ const input = require("fs").readFileSync("/dev/stdin", "utf8");
 const lines = input.split(/\n|\s/);
 
 const monthHash = {
-  1: "Janary",
+  1: "January",
   2: "February",
   3: "March",
   4: "April",
   5: "May",
   6: "June",
   7: "July",
-  8: "Agust",
+  8: "August",
   9: "September",
-  10: "Octuber",
+  10: "October",
   11: "November",
   12: "December",
 };
