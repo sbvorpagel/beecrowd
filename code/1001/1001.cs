2aede3d0652d86d79753f3df468bb3ac6cf6c0a8
sbvorpagel <sbvorpagel@gmail.com>
2023-07-07 20:19:48 -0300

    Added 1001 C# soluction

---
+++ code/1001/1001.cs
@@ -0,0 +1,10 @@
+using System;
+
+class URI {
+  static void Main(string[] args) {
+    int num1, num2;
+    int.TryParse(Console.ReadLine(), out num1);
+    int.TryParse(Console.ReadLine(), out num2);
+    Console.WriteLine("X = " + (num1 + num2));
+  }
+}
