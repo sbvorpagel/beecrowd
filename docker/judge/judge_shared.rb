6d3b24a8df002ef1e84fe6e27fc79ed60d9d566f
sbvorpagel <sbvorpagel@gmail.com>
2023-07-08 08:51:53 -0300

    Added return in judge_shared and remove comment step and ci.

--- docker/judge/judge_shared.rb
+++ docker/judge/judge_shared.rb
@@ -4,16 +4,20 @@ def execution_code(command, input_path)
   `#{cli_code}`
 end
 
-def feedback(language, execution, expected)
-  if execution == expected
-    puts "#{language} is ok!"
-  else
-    puts "#{language} isn't ok!"
-    puts 'Expected: '
-    puts expected
-    puts 'Receive: '
-    puts execution
-  end
+def wrong_test(number, language, execution, expected)
+  puts "Test #{number} for #{language} failed"
+  puts 'Expected: '
+  puts expected
+  puts 'Receive: '
+  puts execution
+end
+
+def correct_test(number, language)
+  puts "Test #{number} for #{language} passed"
+end
+
+def compare_results(execution, expected)
+  execution == expected
 end
 
 def get_output_data(path)
@@ -34,10 +38,21 @@ def check(path, language, commannd_string)
   index_input_and_output = 1
   output_and_input_path = get_output_and_input_path(path, index_input_and_output)
 
+  all_very_well = true
+
   while File.exist?(output_and_input_path['output'])
     execution_output = execution_code(commannd_string, output_and_input_path['input'])
-    feedback(language, execution_output, get_output_data(output_and_input_path['output']))
+    if !compare_results(execution_output, get_output_data(output_and_input_path['output']))
+      all_very_well = false
+      wrong_test(path, language, execution_output, get_output_data(output_and_input_path['output']))
+    end
     index_input_and_output += 1
     output_and_input_path = get_output_and_input_path(path, index_input_and_output)
   end
+
+  if all_very_well
+    correct_test(path, language)
+    return 0
+  end
+  return 1
 end
