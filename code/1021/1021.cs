6ab4e01736a0543d8a1b3f9db8f7f58bb3260ff7
sbvorpagel <sbvorpagel@gmail.com>
2023-07-10 10:58:53 -0300

    Added 1021 c# solution

--- code/1021/1021.cs
+++ code/1021/1021.cs
@@ -4,9 +4,9 @@ class URI
 {
     static void Main(string[] args)
     {
-        double monetary_value;
+        decimal monetary_value;
 
-        monetary_value = double.Parse(Console.ReadLine());
+        monetary_value = decimal.Parse(Console.ReadLine());
 
         Console.WriteLine("NOTAS:");
         Console.WriteLine("{0} nota(s) de R$ 100.00", (int)monetary_value / 100);
@@ -22,16 +22,17 @@ class URI
         Console.WriteLine("{0} nota(s) de R$ 2.00", (int)monetary_value / 2);
         monetary_value %= 2;
         Console.WriteLine("MOEDAS:");
-        Console.WriteLine("{0} moeda(s) de R$ 1.00", (int)monetary_value / 1);
+        Console.WriteLine("{0} moeda(s) de R$ 1.00", (int)monetary_value);
         monetary_value %= 1;
-        Console.WriteLine("{0} moeda(s) de R$ 0.50", (int)(monetary_value / 0.50));
-        monetary_value %= 0.50;
-        Console.WriteLine("{0} moeda(s) de R$ 0.25", (int)(monetary_value / 0.25));
-        monetary_value %= 0.25;
-        Console.WriteLine("{0} moeda(s) de R$ 0.10", (int)(monetary_value / 0.10));
-        monetary_value %= 0.10;
-        Console.WriteLine("{0} moeda(s) de R$ 0.05", (int)(monetary_value / 0.05));
-        monetary_value %= 0.05;
-        Console.WriteLine("{0} moeda(s) de R$ 0.01", (int)(monetary_value / 0.01));
+        Console.WriteLine("{0} moeda(s) de R$ 0.50", (int)(monetary_value / 0.5m));
+        monetary_value %= 0.5m;
+        Console.WriteLine("{0} moeda(s) de R$ 0.25", (int)(monetary_value / 0.25m));
+        monetary_value %= 0.25m;
+        Console.WriteLine("{0} moeda(s) de R$ 0.10", (int)(monetary_value / 0.1m));
+        monetary_value %= 0.1m;
+        Console.WriteLine("{0} moeda(s) de R$ 0.05", (int)(monetary_value / 0.05m));
+        monetary_value %= 0.05m;
+        Console.WriteLine("{0} moeda(s) de R$ 0.01", (int)(monetary_value / 0.01m));
+       
     }
 }
