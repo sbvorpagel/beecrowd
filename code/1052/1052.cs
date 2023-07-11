239b71c86cadf38e7e48223c7e05e23e6e0ef82b
sbvorpagel <sbvorpagel@gmail.com>
2023-07-11 09:06:20 -0300

    Added 1052 c# solution.

---
+++ code/1052/1052.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections.Generic;
+
+class URI
+{
+    static void Main(string[] args)
+    {
+        Dictionary<int, string> months = new Dictionary<int, string>()
+        {
+            { 1, "January" },
+            { 2, "February" },
+            { 3, "March" },
+            { 4, "April" },
+            { 5, "May" },
+            { 6, "June" },
+            { 7, "July" },
+            { 8, "August" },
+            { 9, "September" },
+            { 10, "October" },
+            { 11, "November" },
+            { 12, "December" }
+        };
+
+        int input = int.Parse(Console.ReadLine());
+
+        Console.WriteLine(months[input]);
+    }
+}
