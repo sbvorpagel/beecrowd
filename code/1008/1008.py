de64978c495b096bcd90ed5f2e75fec7dc9613e9
sbvorpagel <sbvorpagel@gmail.com>
2023-12-26 15:00:11 -0300

    Added 1008 python solution.

---
+++ code/1008/1008.py
@@ -0,0 +1,6 @@
+employeeNumber = int(input())
+employeeHours = int(input())
+employeeSalary = float(input())
+
+print("NUMBER =", employeeNumber)
+print("SALARY = U$ %.2f" % (employeeHours * employeeSalary))
