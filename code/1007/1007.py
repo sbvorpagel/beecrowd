e89dc52a1a9a5e102f99d87672356af6a9015c55
sbvorpagel <sbvorpagel@gmail.com>
2023-12-26 15:00:04 -0300

    Added 1007 python solution.

---
+++ code/1007/1007.py
@@ -0,0 +1,8 @@
+value_A = int(input())
+value_B = int(input())
+value_C = int(input())
+value_D = int(input())
+
+value_calculated = value_A * value_B - value_C * value_D
+
+print("DIFERENCA = {:d}".format(value_calculated))
