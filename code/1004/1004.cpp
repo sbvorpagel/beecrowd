bad373eeb93aecc4033fabbebc76d7ab84c009d3
sbvorpagel <sbvorpagel@gmail.com>
2023-12-26 23:32:13 -0300

    Added 1004 c++ solution.

---
+++ code/1004/1004.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int numberOne, numberTwo;
+    cin >> numberOne >> numberTwo;
+    cout << "PROD = " << numberOne * numberTwo << endl;
+    return 0;
+}
