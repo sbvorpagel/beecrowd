fb7d4760a79eced542303f49445b597a84b8db30
sbvorpagel <sbvorpagel@gmail.com>
2023-12-26 23:32:29 -0300

    Added 1006 c++ solution.

---
+++ code/1006/1006.cpp
@@ -0,0 +1,11 @@
+#include <iomanip>
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    double a, b, c;
+    cin >> a >> b >> c;
+    cout << "MEDIA = " << fixed << setprecision(1) << (a * 2 + b * 3 + c * 5) / 10 << endl;
+    return 0;
+}
